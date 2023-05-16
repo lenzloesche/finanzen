b0058c0edd07b0cad4b69bac381ad92c0de97811
Lenz Loesche <lenzloesche@gmail.com>
2023-05-16 13:46:48 +0200

    add test inputForm

--- components/InputForm/inputForm.test.js
+++ components/InputForm/inputForm.test.js
@@ -1,6 +1,18 @@
+import dataPrototype from "@/utils/data/dataPrototype";
 import InputForm from ".";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import startingInput from "@/utils/data/starting-input";
 
-test("there are 2 input fields for each category", () => {
-  render(<InputForm handleSubmit={()=>{}} inputFields, setInputFields={()=>{}} categories={10} valueSumIst={10} valueSum={10}/>);
+test("there are 2 input fields for each category and have a name-text. also there is a SOLL and IST text.", () => {
+  render(<InputForm handleSubmit={() => {}} inputFields={startingInput} setInputFields={() => {}} categories={dataPrototype} valueSumIst={10} valueSum={10} />);
+  const allInputFields = screen.getAllByRole("spinbutton");
+  expect(allInputFields.length).toBe(10);
+  const label1 = screen.getAllByText(/Einkauf/i);
+  expect(label1[0]).toBeInTheDocument();
+  const label2 = screen.getAllByText(/Miete/i);
+  expect(label2[0]).toBeInTheDocument();
+  const title1 = screen.getByText(/SOLL/i);
+  expect(title1).toBeInTheDocument();
+  const title2 = screen.getByText(/IST/i);
+  expect(title2).toBeInTheDocument();
 });

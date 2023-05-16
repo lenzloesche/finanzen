57c03f146800371bd1303a6dcb2afad018100e12
Lenz Loesche <lenzloesche@gmail.com>
2023-05-16 13:20:55 +0200

    test for calendar

--- __tests__/expenses-graph.test.js
+++ __tests__/expenses-graph.test.js
@@ -2,10 +2,12 @@ import ExpensesGraph from "@/pages/expenses-graph";
 import { render, screen } from "@testing-library/react";
 import startingInput from "@/utils/data/starting-input";
 import dataPrototype from "@/utils/data/dataPrototype";
-import ResizeObserver from "../__mocks__/resizeObserver";
+import ResizeObserver from "../__mocks__/resizeObserver"; //is needed!
 
-test("there is a navbar with buttons", () => {
+test("there is a text for the graphes IST and SOLL", () => {
   render(<ExpensesGraph data={startingInput} dataPrototype={dataPrototype} currentYear={0} />);
-  const indexLink = screen.getByText("IST");
-  expect(indexLink).toBeInTheDocument();
+  const istText = screen.getByText("IST");
+  expect(istText).toBeInTheDocument();
+  const sollText = screen.getByText("SOLL");
+  expect(sollText).toBeInTheDocument();
 });
